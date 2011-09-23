f81d0848a76ab1d3ad3543cb6c5dabe338c01d18
Damian Carrillo <dcarrillo@ironclad.mobi>
2011-09-23 16:53:27 -0500

    Adding shell script to run tests
    Adding a shell script to Invoke the UI Automation tests

---
+++ mother.sh
@@ -0,0 +1,215 @@
+#! /usr/bin/env bash
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+# mother.sh
+#
+#   Invokes the UI Automation tests using the Mother-May-UI javascript test
+#   framework.
+#
+# Example(s):
+#
+#   $ ./mother.sh -i 1341234124
+#                 -a Sample.app \
+#                 -o outputDir 
+#                 -v
+#                 testFile.js
+# 
+# Discussion:
+#
+#   This shell script invokes `instruments' so that the supplied UI Automation
+#   tests can be run against the device identified by the UDID (specified as
+#   the argument to -i) is available from the Xcode's Organizer.  If the -i 
+#   option is specified and the device is plugged in, this script will run 
+#   the tests using the referenced device. If it is not supplied, this script 
+#   will run the tests using the simulator. 
+#
+#   You can specify the path to the app, however, the specified app must
+#   be able to be run on the specified device.  If you only give the name of 
+#   the app (as in Sample.app), this script will attempt to find it.  If you 
+#   specif a UDID, this script will attempt to find the app in the current
+#   user's Xcode build directory.  In particular, the following directory 
+#   will be sought out:
+#
+#     ~/Library/Developer/Xcode/DerivedData/.../Debug-iphoneos/Sample.app
+#
+#   If there are multiple app bundles that match the value of the -a 
+#   argument, the most recently touched one will be used.  This behavior
+#   does not occur when you explicitly name the app bundle to use.
+#
+#   The results of this script end up in the output directory under a 
+#   timestamped subdirectory.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+app=
+deviceID=
+outputDir=
+targetApp=
+traceTemplate=
+command="instruments"
+verbose=0
+testFile=
+dateTime=$(date +%Y-%m-%dT%H:%M:%S)
+usage="Usage: ${0} -i <device ID> -a <app bundle> -o <output dir> [-v] <test file>"
+
+function lastModifiedTime {
+  dir=${1}
+  echo $(find "${1}" -exec stat -f "%m" \{} \; | sort -n -r | head -1)
+}
+
+
+# Options
+
+while getopts ":a:i:t:o:v" opt
+do
+  case ${opt} in
+    a)
+      app=${OPTARG}
+      ;;
+    i)
+      deviceID=${OPTARG}
+      ;;
+    o)
+      outputDir="${OPTARG}/${dateTime}"
+      ;;
+    v)
+      verbose=1
+      ;;
+    ?)
+      echo "${usage}"
+      exit 1
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+if [[ -z "${app}" || -z "${outputDir}" ]]
+then
+  echo "${usage}"
+  exit 1
+fi
+
+
+# Device ID
+
+if [[ ! -z "${deviceID}" ]]
+then
+  command="${command} -w \"${deviceID}\""
+fi
+
+
+# Trace Template
+
+traceTemplate=$(find /Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments -type f -name "Automation.tracetemplate")
+
+if [[ -z "${traceTemplate}" ]]
+then
+  echo "Error: Could not find Automation.tracetemplate" >&2
+  exit 1
+else
+  command="${command} -t \"${traceTemplate}\""
+fi
+
+
+# Target App
+
+if [[ -d ${app} ]]
+then
+  targetApp=${app}
+else
+  mostRecentlyModifiedTime=
+  environment=
+
+  if [[ -z "${deviceID}" ]]
+  then
+    environment="iphonesimulator"
+  else
+    environment="iphoneos"
+  fi
+
+  for potentialApp in $(find ~/Library/Developer/Xcode/DerivedData -type d -path "*/Build/Products/Debug-${environment}/${app}")
+  do
+    if [[ -n "${modifiedTime}" ]]
+    then
+      mostRecentlyModifiedTime=$(lastModifiedTime ${potentialApp})
+      targetApp=${potentialApp}
+    fi
+
+    modifiedTime=$(lastModifiedTime ${potentialApp})
+
+    if [[ ${modifiedTime} > ${mostRecentlyModifiedTime} ]]
+    then
+      mostRecentlyModifiedTime=${modifiedTime}
+      targetApp=${potentialApp}
+    fi
+  done
+fi
+
+if [[ -z "${targetApp}" ]]
+then
+  echo "Error: Could not find ${app}" >&2
+  exit 1
+else
+  command="${command} ${targetApp}"
+fi
+
+
+# Output Directory
+
+if [[ ! -d "${outputDir}" ]]
+then
+  mkdir -p "${outputDir}"
+fi
+
+command="${command} -e UIARESULTSPATH \"${outputDir}\""
+
+
+# Trace Document
+# instruments doesn't currently seem to respect this
+
+command="${command} -d \"${outputDir}/mother.trace\""
+
+
+# Test File
+
+if [[ -z "$*" ]]
+then
+  echo "Error: No test scripts were supplied"
+  exit 1
+else
+  for scriptFile in "$*"
+  do
+    testFile="${scriptFile}"
+  done
+
+  command="${command} -e UIASCRIPT \"${testFile}\""
+fi
+
+if (( ${verbose} > 0))
+then
+  echo "${command}"
+fi
+
+
+# Workaround the output trace fileit by just changing to the output directory so any output ends up in there
+
+pushd . > /dev/null
+tempOutputDir="${outputDir}/tmp-$$"
+mkdir "${tempOutputDir}" > /dev/null
+pushd "${tempOutputDir}" > /dev/null
+
+eval ${command}
+
+for tempTraceDocument in $(ls ${tempOutputDir})
+do
+  finalTraceDocument="${outputDir}/${0%.*}.trace"
+  mv "${tempTraceDocument}" "${finalTraceDocument}"
+done
+
+rm -rf "${tempOutputDir}"
+
+# Restore the working directory
+
+popd > /dev/null
+
+echo "Finished: Results are located in ${outputDir}"

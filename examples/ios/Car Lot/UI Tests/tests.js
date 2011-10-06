cf76eff8dd95ad1c45af57d812e7f120feb64ac5
Brandon Alexander <baalexander@gmail.com>
2011-10-06 16:14:14 -0500

    Adds semicolons to test file.
    Keeps consistent with rest of Mother's JavaScript by using semicolons.

--- examples/ios/Car Lot/UI Tests/tests.js
+++ examples/ios/Car Lot/UI Tests/tests.js
@@ -14,26 +14,26 @@
 
 // #import is an iOS UI Automation provided macro. Links to the mother-ios.js
 // header file.
-#import "../../../../mother-ios.js"
+#import "../../../../mother-ios.js";
 
 // Verbose logging will print out each test case inside a scenario. If false,
 // only the final result of each scenario is printed.
-mother.config.verbose = true
+mother.config.verbose = true;
 // Will take a screenshot on every test case (usually corresponding a to a view)
-mother.config.takeScreenshot = mother.config.takeScreenshotOptions.ALWAYS
+mother.config.takeScreenshot = mother.config.takeScreenshotOptions.ALWAYS;
 
 // A setUp function is called at the beginning of each scenario. Mother provides
 // a default setUp for iOS apps. We override it here to customize the UI
 // Automation timeout parameter.
 mother.setUp = function() {
   // Target, App, and MainWindow are also provided by Mother's default setUp.
-  this.target     = UIATarget.localTarget()
-  this.app        = this.target.frontMostApp()
-  this.mainWindow = this.app.mainWindow()
+  this.target     = UIATarget.localTarget();
+  this.app        = this.target.frontMostApp();
+  this.mainWindow = this.app.mainWindow();
   // Changes the default timeout from 5 to 2. Timeout is used in wait for
   // invalid. A smaller timeout will transition between views quicker.
-  this.target.setTimeout(2)
-}
+  this.target.setTimeout(2);
+};
 
 // A tearDown function is called at the end of each scenario. You can override
 // the default tearDown as well:
@@ -50,33 +50,33 @@ mother.may.I('Log in and log out as testUser')
   .and('log in as testUser', function() {
     // Retrieves the first text field in the view
     // Note that this.mainWindow is provided by mother's setUp().
-    var usernameField = this.mainWindow.textFields()[0]
+    var usernameField = this.mainWindow.textFields()[0];
     // Sets the text to user 'testUser'
-    usernameField.setValue('testUser')
+    usernameField.setValue('testUser');
     // Retrieves the first password field in the view
-    var passwordField = this.mainWindow.secureTextFields()[0]
-    passwordField.setValue('testPassword')
+    var passwordField = this.mainWindow.secureTextFields()[0];
+    passwordField.setValue('testPassword');
     // Gets the Log In button
-    var loginButton = this.mainWindow.buttons()[0]
-    loginButton.tap()
+    var loginButton = this.mainWindow.buttons()[0];
+    loginButton.tap();
     // Do not transition to the next test case until the new view has loaded
-    passwordField.waitForInvalid()
+    passwordField.waitForInvalid();
   })
   // The next test case in the scenario logs the user out ()
   .and('log out', function() {
     // As this is a super simple app, logging out simply means pressing the back
     // button to the home screen.
-    var backButton = this.mainWindow.navigationBar().leftButton()
+    var backButton = this.mainWindow.navigationBar().leftButton();
     // The util module is provided by the Mother May UI framework. It waits
     // until the backButton becomes valid (clickable)
-    util.waitFor(backButton)
+    util.waitFor(backButton);
     // Assert the back button exists. If not, throw an exception and fail this
     // scenario
-    assert.isTrue(backButton.isValid(), 'Unable to find Back button')
+    assert.isTrue(backButton.isValid(), 'Unable to find Back button');
     // Go back a view
-    backButton.tap()
-    backButton.waitForInvalid()
-  })
+    backButton.tap();
+    backButton.waitForInvalid();
+  });
 
 // Adds another scenario to test. Things get interesting with test case re-use.
 mother.may.I('View details of the Aston Martin')
@@ -85,31 +85,31 @@ mother.may.I('View details of the Aston Martin')
   .and('log in as testUser')
   .and('select the Aston Martin', function() {
     // Reference the UITableView
-    var tableView = this.mainWindow.elements()[1]
-    assert.isTrue(tableView.isValid(), 'Unable to find cars table view')
+    var tableView = this.mainWindow.elements()[1];
+    assert.isTrue(tableView.isValid(), 'Unable to find cars table view');
     // The cell is accessed based on the cell's accessibilityLabel property.
     // This is set in the view controller.
-    var cell = tableView.cells().firstWithName('2012 Aston Martin Virage')
-    tableView.scrollToElementWithName(cell)
-    cell.tap()
-    tableView.waitForInvalid()
+    var cell = tableView.cells().firstWithName('2012 Aston Martin Virage');
+    tableView.scrollToElementWithName(cell);
+    cell.tap();
+    tableView.waitForInvalid();
   })
   .and('verify Aston Martin details', function() {
     // Validate the values
   })
   .and('back out to cars list', function() {
-    var backButton = this.mainWindow.navigationBar().leftButton()
-    assert.isTrue(backButton.isValid(), 'Unable to find Back button')
-    backButton.tap()
-    backButton.waitForInvalid()
+    var backButton = this.mainWindow.navigationBar().leftButton();
+    assert.isTrue(backButton.isValid(), 'Unable to find Back button');
+    backButton.tap();
+    backButton.waitForInvalid();
   })
   // We can re-use the log out test case too
-  .and('log out')
+  .and('log out');
 
 // Calling please on mother executes each scenario in order until one fails or
 // they all pass.
 // If you only want to execute a single scenario, you can call please on a
 // specific scenario instead, like so:
 // mother.may.I('awesome scenario').and('do something').please()
-mother.please()
+mother.please();
 
